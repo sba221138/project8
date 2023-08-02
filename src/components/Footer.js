55e6c71abfa5db8e61e8bdb8a16ac3c49fa85d9f
sba221138 <thunder.alchemist.21@gmail.com>
2023-08-02 20:13:00 +0800

    setting ignore

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className="footer">Bill Kuo 2023</div>;
+};
+
+export default Footer;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect, useInsertionEffect } from "react";
+import Search from "../components/Search";
+import axios from "axios";
+import Picture from "../components/Picture";
+
+const Homepage = () => {
+  let [input, setInput] = useState("");
+  let [data, setDate] = useState(null);
+  let [page, setPage] = useState(1);
+  let [currentSearch, setCurrentSearch] = useState("");
+  const auth = process.env.REACT_APP_AUTH;
+  const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
+  let searchURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`;
+
+  const search = async (url) => {
+    let result = await axios.get(url, {
+      headers: { Authorization: auth },
+    });
+    console.log(result.data.photos);
+    setDate(result.data.photos);
+    setCurrentSearch(input);
+  };
+
+  const morePicture = async () => {
+    let newURL;
+    setPage(page + 1);
+    if (currentSearch === "") {
+      newURL = `https://api.pexels.com/v1/curated?page=${page}&per_page=15`;
+    } else {
+      newURL = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${page}`;
+    }
+    let result = await axios.get(newURL, {
+      headers: { Authorization: auth },
+    });
+    setDate(data.concat(result.data.photos));
+  };
+
+  useEffect(() => {
+    search(initialURL);
+  }, []);
+
+  return (
+    <div style={{ minHeight: "100vh" }}>
+      <Search
+        search={() => {
+          search(searchURL);
+        }}
+        setInput={setInput}
+      />
+      <div className="pictures">
+        {data &&
+          data.map((d) => {
+            return <Picture data={d} />;
+          })}
+      </div>
+      <div className="morePicture">
+        <button onClick={morePicture}>更多圖片</button>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
